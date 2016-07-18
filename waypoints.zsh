a3bf2f8b4b353c9027a04465253245ae20bee222
Anthony Nguyen <anknguyen@gmail.com>
2016-07-18 12:10:04 -0400

    Remove wps command and merge it into wp

--- waypoints.zsh
+++ waypoints.zsh
@@ -1,9 +1,16 @@
 export WAYPOINTS=$HOME/.waypoints
 
-function wp { 
-	cd -P "$WAYPOINTS/$1" 2>/dev/null
-	if [[ $? -ne 0 ]]; then
-		echo "No '"$1"' waypoint"
+function wp {
+	if [[ "$#" -ne 1 ]]; then
+		mkdir -p $WAYPOINTS
+		for f in $(find $WAYPOINTS -type l -maxdepth 1 -exec basename {} \;); do
+			echo $f "->" $(readlink "$WAYPOINTS/$f")
+		done
+	else
+		cd -P "$WAYPOINTS/$1" 2>/dev/null
+		if [[ $? -ne 0 ]]; then
+			echo "No '"$1"' waypoint"
+		fi
 	fi
 }
 
@@ -14,11 +21,4 @@ function mwp {
 
 function dwp { 
 	rm -i "$WAYPOINTS/$1"
-}
-
-function wps {
-	mkdir -p $WAYPOINTS
-	for f in $(find $WAYPOINTS -type l -maxdepth 1 -exec basename {} \;); do
-		echo $f "->" $(readlink "$WAYPOINTS/$f")
-	done
 }
