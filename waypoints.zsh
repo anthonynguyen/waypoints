0b723b25a5bb3f1bc923f2e9533b18c385d34541
Anthony Nguyen <anknguyen@gmail.com>
2016-07-18 11:53:40 -0400

    Initial commit

---
+++ waypoints.zsh
@@ -0,0 +1,24 @@
+export WAYPOINTS=$HOME/.waypoints
+
+function wp { 
+	cd -P "$WAYPOINTS/$1" 2>/dev/null
+	if [[ $? -ne 0 ]]; then
+		echo "No '"$1"' waypoint"
+	fi
+}
+
+function mwp { 
+	mkdir -p $WAYPOINTS
+	ln -s "$(pwd -P)" "$WAYPOINTS/$1"
+}
+
+function dwp { 
+	rm -i "$WAYPOINTS/$1"
+}
+
+function wps {
+	mkdir -p $WAYPOINTS
+	for f in $(find $WAYPOINTS -type l -maxdepth 1); do
+		echo $(basename $f) "->" $(readlink $f)
+	done
+}
